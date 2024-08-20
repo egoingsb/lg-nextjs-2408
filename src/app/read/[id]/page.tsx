6dabff516cf4e3e762f1d94994d9382a3c35a97d
egoing <egoing@gmail.com>
2024-08-20 15:53:23 +0900

    Read 구현

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,10 +1,30 @@
 type ReadPropsType = {
     params: {
         id: string;
+    };
+};
+
+type TopicType = {
+    id: string;
+    title: string;
+    body: string;
+};
+
+async function fetchTopic(id: string): Promise<TopicType> {
+    const res = await fetch(`http://localhost:9999/topics/${id}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
     }
+    return res.json();
 }
-export default function Read(props:ReadPropsType){
-    return <>
-        <h2>Read {props.params.id}</h2>
-    </>
+
+export default async function Read({ params }: ReadPropsType) {
+    const topic = await fetchTopic(params.id);
+
+    return (
+        <>
+            <h2>{topic.title}</h2>
+            <p>{topic.body}</p>
+        </>
+    );
 }
