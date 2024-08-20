9f40d5ca77934be49796fe8f9f3e2b078c73e912
egoing <egoing@gmail.com>
2024-08-20 13:33:46 +0900

    기본적인 골격을 담고 있는 마크업 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello, Nextjs!</div>
+    <div>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+      </ol>
+      <h2>Welcome</h2>
+      Hello, Next
+      <ul>
+        <li><a href="/create">create</a></li>
+      </ul>
+    </div>
   );
 }
