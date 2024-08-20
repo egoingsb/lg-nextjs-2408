36398ad2e380abfafa5a854aa909a68ebc40abeb
egoing <egoing@gmail.com>
2024-08-20 16:49:36 +0900

    사용자가 상호작용하는 js를 클라이언트로 전송할 때는 클라이언트 컴포넌트를 사용한다.
    클라이언트 컴포넌트는 'use client' 지시자로 시작한다.
    
    useState, useEffect, event handler를 사용할 수 있게 된다.
    
    useRouter를 이용해서 router 객체를 가져온다.
    
    router는 리디렉션, refresh 등의 작업을 처리 할 수 있다.

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,39 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
 export default function Create(){
+    const router = useRouter();
+    // @ts-ignore
+    const submitHandler = async (evt)=>{
+        evt.preventDefault();
+        const title = evt.target._title.value;
+        const body = evt.target._body.value;
+        const option = {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title, 
+                body
+            })
+        }
+        const resp = await fetch('http://localhost:9999/topics', option)
+        const result = await resp.json();
+        router.push(`/read/${result.id}`);
+        router.refresh();
+    }
     return <>
         <h2>Create</h2>
+        <form onSubmit={submitHandler}>
+            <p><input type="text" name="_title" placeholder="title" /></p>
+            <p>
+                <textarea name="_body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Create" />
+            </p>
+        </form>
     </>
 }
